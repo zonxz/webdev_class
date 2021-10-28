462299e9aca401627b57e17390ff8792cbdd3283
Skyote <c-83@MTTOP.socal.rr.com>
2021-10-27 23:55:04 -0700

    stingfy JSON OBj and pass random string server side

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
 
  const express = require("express");
  const path = require("path");
-
+ var lemon = require("./script/test.js");
 /**
  * App Variables
  */
@@ -28,11 +28,30 @@
  app.use(express.static(path.join(__dirname, "public")));
 
 /*
+*   Testing server side scripting and interpolation
+*/
+
+var username = "Mr. Testname:'I was declared server side i got '";
+var me = lemon.getMe;
+
+
+
+/*
  * Routes Definitions
  */
  app.get("/", (req, res) => {
-   res.render("index", { title: "Home" });
+   res.render("index", { title: "Home"});
  });
+ app.get("/user", (req, res) => {
+  res.render("user", { title: "Profile", userProfile:{nickname: "Caleb"} });
+});
+app.get("/logout", (req, res) => {
+  res.render("logout", { title: "LoggedOut",user:username,usrlem:me });
+  
+});
+app.get("/test", (req, res) => {
+  res.render("test", { title: "test" });
+});
 
 /*
  app.get("/", (req, res) => {
